6f6c5b8d26bc095b1bb1634cb607ac4f95876a54
3y3k0 <3y3k0@yandex-team.ru>
2019-10-07 20:57:09 +0300

    Extract BasePlugin

--- src/plugins/ScriptAttr/index.js
+++ src/plugins/ScriptAttr/index.js
@@ -1,88 +1,16 @@
 'use strict';
 
-class ScriptAttrPlugin {
+const BasePlugin = require('../BasePlugin');
 
-    constructor(name, options, optionsResolver, codeResolver) {
-        this.options = options;
-        this.pluginName = `ScriptAttr(${name})`;
+class ScriptAttrPlugin extends BasePlugin {
 
-        if (optionsResolver) {
-            this.optionsResolver = optionsResolver
-        }
-
-        if (codeResolver) {
-            this.codeResolver = codeResolver;
-        }
+    constructor(name, options) {
+        super(
+            `ScriptAttr(${name})`,
+            options,
+            'scriptOptionsResolverStrategy'
+        );
     }
-
-    apply(compiler) {
-        compiler.hooks.compilation.tap(this.pluginName, compilation => {
-            const { mainTemplate } = compilation;
-
-            const options = this.optionsResolver(compilation);
-
-            if (!options) {
-                return;
-            }
-
-            this.setupExtraHooks(compilation, options);
-
-            mainTemplate.hooks.localVars.tap(
-                this.pluginName,
-                (source, chunk, hash) => {
-                    const code = this.localVarsResolver(options, chunk, hash);
-
-                    if (code) {
-                        source += '\n' + code;
-                    }
-
-                    return source;
-                }
-            );
-
-            mainTemplate.hooks.scriptOptionsResolverStrategy.tap(
-                this.pluginName,
-                (array, chunk, hash, expressions) => {
-                    const code = this.codeResolver(options, chunk, hash, expressions);
-
-                    if (code) {
-                        array.push(code);
-                    }
-
-                    return array;
-                }
-            );
-        });
-    }
-
-    /**
-     * @virtual
-     * @protected
-     * @returns {?Object}
-     */
-    optionsResolver(compilation) {
-        return {};
-    }
-
-    /**
-     * @virtual
-     * @protected
-     */
-    setupExtraHooks(compilation, options) {}
-
-    /**
-     * @virtual
-     * @protected
-     * @returns {?String}
-     */
-    localVarsResolver(options, chunk, hash) {}
-
-    /**
-     * @virtual
-     * @protected
-     * @returns {?String}
-     */
-    codeResolver(options, chunk, hash, expressions) {}
 }
 
 module.exports = ScriptAttrPlugin;
