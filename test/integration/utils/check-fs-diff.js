b4197d902dc75dd6f6d0e8b7af86cbec2012e96c
3y3k0 <3y3k0@yandex-team.ru>
2019-10-09 15:51:52 +0300

    Optimize test snapshots

--- test/integration/utils/check-fs-diff.js
+++ test/integration/utils/check-fs-diff.js
@@ -12,13 +12,27 @@ module.exports = function(base, curr) {
     expect(baseDir).toEqual(currDir);
 
     currDir.forEach((file) => {
+        let changeIndex = 1;
+
         const linesDiff = diff.createTwoFilesPatch(
             file, file,
             base.fs.readFileSync(mainOutputDir + '/' + file).toString(),
             curr.fs.readFileSync(mainOutputDir + '/' + file).toString(),
             '', '',
             { context: 0 }
+        ).replace(
+            /@@ \-\d+,\d+? \+\d+,\d+? @@/g,
+            () => `@@ Change ${changeIndex++} @@`
         );
+
+        // Ignore diff like this:
+        //       Index: test5.js
+        //       ===================================================================
+        //       --- test5.js
+        //       +++ test5.js
+        if (linesDiff.match(/^Index:\s(.*?)\n[=]+\n[-]{3}\s\1\s*\n[+]{3}\s\1\s*$/)) {
+            return;
+        }
 
         expect(linesDiff).toMatchSnapshot(file);
     });
